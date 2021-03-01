b5e0e432ad242f7229ac32915da276b0905689f6
Poke711 <Poke711@hotmail.com>
2021-03-01 11:30:18 -0700

    Added Clock model

--- app/src/main/java/com/example/Models/Clock.java
+++ app/src/main/java/com/example/Models/Clock.java
@@ -1,6 +1,20 @@
+package com.example.Models;
 
-public class clock {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.javaclock.R;
+
+public class Clock extends AppCompatActivity {
 
 
+    public void start(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clock);
+    }
+    public void newFunction(){
+
+    }
 
 }
