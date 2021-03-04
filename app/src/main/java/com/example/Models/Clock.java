0fec9288b2f8cd4ca98d3fa650c8efc703d72c22
Poke711 <Poke711@hotmail.com>
2021-03-03 21:02:47 -0700

    Added Clock function but it doesn't update yet Added weekDay and date to the UI

--- app/src/main/java/com/example/Models/Clock.java
+++ app/src/main/java/com/example/Models/Clock.java
@@ -1,28 +1,35 @@
 package com.example.Models;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import com.example.javaclock.R;
 
-public class Clock extends AppCompatActivity {
+public class Clock{
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm");
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+    private SimpleDateFormat dayOfWeek = new SimpleDateFormat("EEEE");
+    private SimpleDateFormat seconds = new SimpleDateFormat("ss");
+    private Date systemTime = new Date();
 
+    public String getClockTime(){
+        return timeFormat.format(systemTime);
+    }
+    public String getDate(){
+        return dateFormat.format(systemTime);
+    }
+    public String getWeekDay(){
+        return dayOfWeek.format(systemTime);
+    }
+    public String getSeconds(){
+        System.out.println(seconds.format(systemTime));
+        return seconds.format(systemTime);
+    }
 
-    public void start(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.clock);
-    }
-    private void updateClock(){
-
-    }
-    public void newFunction(){
-
-    }
-    private String getSystemTime(){
-        Date currentTime = Calendar.getInstance().getTime();
-        return currentTime.toString();
-    }
 }
--- app/src/main/java/com/example/javaclock/ClockActivity.java
+++ app/src/main/java/com/example/javaclock/ClockActivity.java
@@ -6,8 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.javaclock.R;
+import android.widget.TextView;
+import com.example.Models.Clock;
 
 public class ClockActivity extends AppCompatActivity {
 
@@ -16,6 +16,8 @@ public class ClockActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.clock);
         Button button = findViewById(R.id.button);
+        whileOpen();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,8 +25,35 @@ public class ClockActivity extends AppCompatActivity {
             }
         });
     }
-    public void openSettings(){
+
+    private void openSettings(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    private void whileOpen(){
+        startClockAndDate();
+    }
+
+    private void startClockAndDate(){
+        final TextView clock = findViewById(R.id.clock);
+        final TextView seconds = findViewById(R.id.seconds);
+        final TextView date = findViewById(R.id.date);
+        final TextView weekDay = findViewById(R.id.Weekday);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                updateClock(clock,seconds,date,weekDay);
+            }
+        });
+    }
+
+    private void updateClock(TextView clockText,TextView secondsText,TextView date, TextView weekDay){
+            Clock clock = new Clock();
+            clockText.setText(clock.getClockTime());
+            secondsText.setText(clock.getSeconds());
+            date.setText(clock.getDate());
+            weekDay.setText(clock.getWeekDay());
+    }
+
 }
